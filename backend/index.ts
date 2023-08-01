f08fc1fd4f4a9cbeee6c27967d61e64e5990a783
roycehoe <royceinbox@hotmail.com>
2023-08-01 15:45:35 +0800

    feat: Create endpoint that returns contents of json file

--- backend/index.ts
+++ backend/index.ts
@@ -7,27 +7,36 @@ const PORT = 3000;
 
 const app = express();
 
+async function getMovieData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.resolve(__dirname, "./data/movie.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      }
+    );
+  });
+}
+
 app.use(
   cors({
     origin: ["*"],
   })
 );
 
-app.get("/", (req, res) => {
-  fs.readFile(
-    path.resolve(__dirname, "./data/movie.json"),
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        // Send error response
-        res.status(500).send("An error occurred while reading the JSON file.");
-      } else {
-        // Parse JSON data and send it as response
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
-      }
-    }
-  );
+app.get("/test", async (req, res) => {
+  const movieData = await getMovieData();
+  console.log(movieData);
+});
+
+app.get("/", async (req, res) => {
+  const movieData = await getMovieData();
+  res.json(movieData);
 });
 
 app.listen(PORT, () => {
