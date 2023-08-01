6ca93d4cdc5299c2cdca8c16f3f33fcecc272460
roycehoe <royceinbox@hotmail.com>
2023-08-01 15:31:25 +0800

    feat: Create endpoint that returns contents of json file

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,35 @@
-console.log("hello world");
+import cors from "cors";
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["*"],
+  })
+);
+
+app.get("/", (req, res) => {
+  fs.readFile(
+    path.resolve(__dirname, "./data/movie.json"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        // Send error response
+        res.status(500).send("An error occurred while reading the JSON file.");
+      } else {
+        // Parse JSON data and send it as response
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
